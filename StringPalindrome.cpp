dd9c0c0f70136846976d60ccc262b7400f25002a
cbhramar <bhramar115@gmail.com>
2017-06-21 03:09:15 +0530

    Adding StringPalindrome.cpp

---
+++ StringPalindrome.cpp
@@ -0,0 +1,32 @@
+/*
+ * File: StringPalindrome.cpp
+ * The program takes in a string as user input and checks whether it is a palindrome or not
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome(string str);
+
+int main() {
+    cout << "Enter the wannabe palindrome \n";
+    string str;
+    cin >> str;
+    if (isPalindrome(str)) {
+        cout << "My, my. This really is a palindrome\n";
+    }
+    else
+        cout << "This one's a dud \n";
+    return 0;
+}
+
+bool isPalindrome(string str) {
+    int n = str.length();
+    for (int i=0; i<(n/2); i++) {
+        if (str[i] != str[n-i-1]) 
+            return false;
+    }
+    return true;
+}
