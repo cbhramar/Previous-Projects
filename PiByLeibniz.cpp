2add9bc489390a458c014b0d41b7f937b46611b7
cbhramar <bhramar115@gmail.com>
2017-06-22 18:55:40 +0530

    Adding PiByLeibniz.cpp

---
+++ PiByLeibniz.cpp
@@ -0,0 +1,26 @@
+/*
+ * File: PiByLeibniz.cpp
+ * The program calculates the value of PI by Leibniz' formula
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int odd(int n);
+
+int main() {
+    long double pi = 0;
+//    long double term = 1;
+    for(int i = 0; i< 10000; i++) {
+        pi = pi + pow(-1, i)*(double(1)/odd(i));
+    }
+    pi = pi*4;
+    cout << pi << endl;
+}
+
+int odd(int n) {
+    int m = 2*n + 1;
+    return m;
+}
