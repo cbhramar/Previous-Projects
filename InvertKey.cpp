7cc413d192a48a52b3e9410b2e8cd773b4320ee7
cbhramar <bhramar115@gmail.com>
2017-06-22 17:57:46 +0530

    Adding InvertKey.cpp

---
+++ InvertKey.cpp
@@ -0,0 +1,34 @@
+/*
+ * File: InvertKey.cpp
+ * The code takes a 26 character encryption key as input and outputs the corresponding decryption key
+ */
+
+#include<iostream>
+#include<string>
+#include<cctype>
+
+using namespace std;
+
+const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+string invertKey(string key);
+
+int main() {
+    cout << "Enter your enryption key :\n";
+    string key;
+    getline(cin, key);
+    cout << "Decription key is:\n" << invertKey(key) << endl;
+    return 0;
+}
+
+string invertKey(string key) {
+    string ikey = ALPHABET;
+    for (int i = 0; i < key.length(); i++) {
+        for (int j = 0; j < ALPHABET.length(); j++) {
+            if (key[i] == ALPHABET[j]) {
+                ikey[j] = ALPHABET[i];
+            }
+        }
+    }
+    return ikey;
+}
